36d4ae669c4cc8c6bcd704790c252199d78ca2c2
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-06-03 15:19:20 -0500

    Guard against weird gitlab/github paths

--- backup_hosted_source_projects.bash
+++ backup_hosted_source_projects.bash
@@ -26,6 +26,10 @@ Main () {
         # Handle simple git repositories
         backup_simple_git
         ;;
+      gitlab.com/*/*/*)
+        Error "Too many parts to gitlab path at line ${line_no}: $url"
+        exit 2
+        ;;
       gitlab.com/*/*)
         # Handle gitlab-hosted projects
         backup_gitlab_project
@@ -33,6 +37,10 @@ Main () {
       gitlab.com/*)
         # Handle gitlab-hosted users and groups
         backup_gitlab_owner
+        ;;
+      github.com/*/*/*)
+        Error "Too many parts to github path at line ${line_no}: $url"
+        exit 2
         ;;
       github.com/*/*)
         # Handle github-hosted repositories
